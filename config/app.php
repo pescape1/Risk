9f61e8fdd0e53d2418fc9e0e328aef911fc19dbc
epulgar01 <elvin.pulgar@gmail.com>
2017-12-20 15:00:18 -0400

    Database to heroku

--- config/app.php
+++ config/app.php
@@ -39,7 +39,7 @@ return [
     |
     */
 
-    'debug' => env('APP_DEBUG', false),
+    'debug' => env('APP_DEBUG', true),
 
     /*
     |--------------------------------------------------------------------------
@@ -104,7 +104,7 @@ return [
     |
     */
 
-    'key' => env('APP_KEY'),
+    'key' => env('APP_KEY','base64:sl5x98rPUTSY+cvibWqK1KTI/WWdbYpG66nNahIM7DU='),
     'cipher' => 'AES-256-CBC',
 
     /*
--- config/database.php
+++ config/database.php
@@ -41,11 +41,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'us-cdbr-iron-east-05.cleardb.net'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'heroku_7ca9f3fed782d44'),
+            'username' => env('DB_USERNAME', 'ba53b421fe4336'),
+            'password' => env('DB_PASSWORD', 'd73dee37'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
